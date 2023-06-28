c7d2a1dc855070d34ad1dcdab54e3b5888e6fe40
InhyeKang <kanginhae0826@gmail.com>
2023-06-28 20:34:13 +0900

    ⚙️ :: 프로젝트 재세팅

---
+++ MYLOG/MYLOG/MYLOGApp.swift
@@ -0,0 +1,17 @@
+//
+//  MYLOGApp.swift
+//  MYLOG
+//
+//  Created by 강인혜 on 28/6/23.
+//
+
+import SwiftUI
+
+@main
+struct MYLOGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
